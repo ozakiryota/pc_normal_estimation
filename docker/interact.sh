e5a1bb7466f4dcbd78b5fc5103db6a0865826fc3
ozakiryota <kb22056@gmail.com>
2022-09-22 23:00:28 +0900

    Refactor

--- docker/interact.sh
+++ docker/interact.sh
@@ -5,7 +5,10 @@ xhost +
 image="pc_normal_estimation"
 tag="latest"
 
-docker run -it --rm \
+docker run \
+	-it \
+	--rm \
+	--net=host \
 	-e "DISPLAY" \
 	-v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	-v $(pwd)/..:/root/catkin_ws/src/$image \
--- src/normal_estimation.cpp
+++ src/normal_estimation.cpp
@@ -1,8 +1,8 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/visualization/cloud_viewer.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl_conversions/pcl_conversions.h>
 
@@ -12,80 +12,70 @@ class NormalEstimation{
 		ros::NodeHandle nh_;
 		ros::NodeHandle nh_private_;
 		/*subscriber*/
-		ros::Subscriber sub_;
+		ros::Subscriber pc_sub_;
 		/*publisher*/
-		ros::Publisher pub_;
-		/*tool*/
-		pcl::visualization::PCLVisualizer viewer {"pc_normal_estimation"};
-		pcl::NormalEstimation<pcl::PointXYZI, pcl::PointXYZINormal> ne_;
-		/*buffer*/
-		pcl::PointCloud<pcl::PointXYZI>::Ptr pc_ {new pcl::PointCloud<pcl::PointXYZI>};
-		pcl::PointCloud<pcl::PointXYZINormal>::Ptr nc_ {new pcl::PointCloud<pcl::PointXYZINormal>};
+		ros::Publisher pc_pub_;
 		/*parameter*/
 		double search_radius_;
 
 	public:
 		NormalEstimation();
 		void callback(const sensor_msgs::PointCloud2ConstPtr& msg);
-		void normalEstimation(void);
-		void visualization(void);
-		void publication(void);
+		template<typename PcPtr, typename PointType, typename NcPtr, typename NormalType, typename KdTreePtr> void normalEstimation(PcPtr pc, PointType no_use_p, NcPtr nc, NormalType no_use_n, sensor_msgs::PointCloud2& pub_pc_msg, KdTreePtr kdtree);
+		void publishMsg(const sensor_msgs::PointCloud2& pub_pc_msg);
 };
 
 NormalEstimation::NormalEstimation()
 	: nh_private_("~")
 {
-	std::cout << "----- normal_estimation -----" << std::endl;
+	std::cout << "----- pc_normal_estimation -----" << std::endl;
 	/*parameter*/
 	nh_private_.param("search_radius", search_radius_, 0.5);
 	std::cout << "search_radius_ = " << search_radius_ << std::endl;
 	/*subscriber*/
-	sub_ = nh_.subscribe("/point_cloud", 1, &NormalEstimation::callback, this);
+	pc_sub_ = nh_.subscribe("/point_cloud", 1, &NormalEstimation::callback, this);
 	/*publisher*/
-	pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/normal_cloud", 1);
-	/*setting*/
-	viewer.setBackgroundColor(1, 1, 1);
-	viewer.addCoordinateSystem(0.5, "axis");
-	ne_.setRadiusSearch(search_radius_);
+	pc_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/normal_cloud", 1);
 }
 
 void NormalEstimation::callback(const sensor_msgs::PointCloud2ConstPtr &msg)
 {
-	pcl::fromROSMsg(*msg, *pc_);
-	copyPointCloud(*pc_, *nc_);
-	normalEstimation();
-	visualization();
-	publication();
+	sensor_msgs::PointCloud2 pub_pc_msg;
+
+	std::string fields = pcl::getFieldsList(*msg);
+	if(fields.find("intensity") != std::string::npos){
+		pcl::PointCloud<pcl::PointXYZI>::Ptr pc (new pcl::PointCloud<pcl::PointXYZI>);
+		pcl::PointCloud<pcl::PointXYZINormal>::Ptr nc (new pcl::PointCloud<pcl::PointXYZINormal>);
+		pcl::fromROSMsg(*msg, *pc);
+		copyPointCloud(*pc, *nc);
+		pcl::search::KdTree<pcl::PointXYZI>::Ptr kdtree (new pcl::search::KdTree<pcl::PointXYZI>());
+		normalEstimation(pc, pc->points[0], nc, nc->points[0], pub_pc_msg, kdtree);
+	}
+	else{
+		pcl::PointCloud<pcl::PointXYZ>::Ptr pc (new pcl::PointCloud<pcl::PointXYZ>);
+		pcl::PointCloud<pcl::PointNormal>::Ptr nc (new pcl::PointCloud<pcl::PointNormal>);
+		pcl::fromROSMsg(*msg, *pc);
+		copyPointCloud(*pc, *nc);
+		pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree (new pcl::search::KdTree<pcl::PointXYZ>());
+		normalEstimation(pc, pc->points[0], nc, nc->points[0], pub_pc_msg, kdtree);
+	}
+	publishMsg(pub_pc_msg);
 }
 
-void NormalEstimation::normalEstimation(void)
+template<typename PcPtr, typename PointType, typename NcPtr, typename NormalType, typename KdTreePtr>
+void NormalEstimation::normalEstimation(PcPtr pc, PointType no_use_p, NcPtr nc, NormalType no_use_n, sensor_msgs::PointCloud2& pub_pc_msg, KdTreePtr kdtree)
 {
-	ne_.setInputCloud(pc_);
-	pcl::search::KdTree<pcl::PointXYZI>::Ptr kdtree_ (new pcl::search::KdTree<pcl::PointXYZI>());
-	ne_.setSearchMethod(kdtree_);
-	ne_.compute(*nc_);
+	pcl::NormalEstimation<PointType, NormalType> ne;
+	ne.setRadiusSearch(search_radius_);
+	ne.setInputCloud(pc);
+	ne.setSearchMethod(kdtree);
+	ne.compute(*nc);
+	pcl::toROSMsg(*nc, pub_pc_msg);
 }
 
-void NormalEstimation::visualization(void)
+void NormalEstimation::publishMsg(const sensor_msgs::PointCloud2& pub_pc_msg)
 {
-	viewer.removeAllPointClouds();
-
-	viewer.addPointCloud<pcl::PointXYZI>(pc_, "pc");
-	viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 0.0, 0.0, 0.0, "pc");
-	viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "pc");
-
-	viewer.addPointCloudNormals<pcl::PointXYZINormal>(nc_, 1, 0.5, "nc");
-	viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1.0, 0.0, 0.0, "nc");
-	viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_LINE_WIDTH, 3, "nc");
-
-	viewer.spinOnce();
-}
-
-void NormalEstimation::publication(void)
-{
-	sensor_msgs::PointCloud2 ros_pc;
-	pcl::toROSMsg(*nc_, ros_pc);
-	pub_.publish(ros_pc);
+	pc_pub_.publish(pub_pc_msg);
 }
 
 int main(int argc, char** argv)
